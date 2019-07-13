6f462f9242aebd43668c8d68af2b06898ff2e649
Evuns <m_ateva@abv.bg>
2019-07-13 17:51:31 +0300

    2.MultipleInheritance

---
+++ src/Inheritance/Lab/MultipleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package Inheritance.Lab.MultipleInheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ src/Inheritance/Lab/MultipleInheritance/Puppy.java
@@ -0,0 +1,7 @@
+package Inheritance.Lab.MultipleInheritance;
+
+public class Puppy extends Dog{
+    public void weep(){
+        System.out.println("weeping...");
+    }
+}
