829c9a314e2fb9fac9c3acf17fe3df066b638cd3
Evuns <m_ateva@abv.bg>
2019-07-16 19:17:08 +0300

    1.ClassBox

--- src/encapsulation/exercise/classBox/Main.java
+++ src/encapsulation/exercise/classBox/Main.java
@@ -9,10 +9,13 @@ public class Main {
         double length = Double.parseDouble(scanner.nextLine());
         double width = Double.parseDouble(scanner.nextLine());
         double height = Double.parseDouble(scanner.nextLine());
-
-        Box box = new Box(length, width, height);
-        System.out.printf("Surface Area - %.2f%n" ,box.calculateSurfaceArea());
-        System.out.printf("Lateral Surface Area - %.2f%n" ,box.calculateLateralSurfaceArea());
-        System.out.printf("Volume - %.2f",box.calculateVolume());
+        try {
+            Box box = new Box(length, width, height);
+            System.out.printf("Surface Area - %.2f%n", box.calculateSurfaceArea());
+            System.out.printf("Lateral Surface Area - %.2f%n", box.calculateLateralSurfaceArea());
+            System.out.printf("Volume - %.2f", box.calculateVolume());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
