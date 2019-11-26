21b64abaddc6398756c7d56910daa132eb8c16b7
Evuns <m_ateva@abv.bg>
2019-11-26 23:16:09 +0200

    1.SystemSplit

---
+++ src/workshop/systemSplit/components/ComponentType.java
@@ -0,0 +1,33 @@
+package workshop.systemSplit.components;
+
+public abstract class ComponentType {
+    private String name;
+    private Enum type;
+    private int someCapacity;
+    private int someMemory;
+
+    public ComponentType(String name, Enum type, int someCapacity, int someMemory) {
+        this.name = name;
+        this.type = type;
+        this.someCapacity = someCapacity;
+        this.someMemory = someMemory;
+    }
+
+//    public Enum getName() {
+//        return name;
+//    }
+//
+//    public Enum getType() {
+//        return type;
+//    }
+
+    public int getSomeCapacity(){
+        return this.someCapacity;
+    }
+
+    public int getSomeMemory(){
+        return this.someMemory;
+    }
+
+}
+
