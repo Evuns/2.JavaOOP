04e3b7fdf84736bfdb7f1fc7aed95e3e70345304
Evuns <m_ateva@abv.bg>
2019-11-29 23:57:20 +0200

    1.SystemSplit

--- src/workshop/systemSplit/system/SystemSplit.java
+++ src/workshop/systemSplit/system/SystemSplit.java
@@ -1,9 +1,12 @@
 package workshop.systemSplit.system;
 
+import workshop.systemSplit.components.ComponentType;
 import workshop.systemSplit.components.Hardware;
+import workshop.systemSplit.components.Software;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.ToIntFunction;
 
 public class SystemSplit {
 
@@ -13,7 +16,51 @@ public class SystemSplit {
         this.hardwareComponents = new HashMap<>();
     }
 
-    public void addHardwareComponent(Hardware hardware){
-        this.hardwareComponents.put(hardware.getName(),hardware);
+    public void addHardwareComponent(Hardware hardware) {
+        this.hardwareComponents.put(hardware.getName(), hardware);
+    }
+
+    public void addSoftwareToHardware(String hardwareName, Software software) {
+        if (this.hardwareComponents.containsKey(hardwareName)) {
+            Hardware hardware = this.hardwareComponents.get(hardwareName);
+            hardware.addSoftware(software);
+        }
+    }
+
+    public void releaseSoftware(String hardwareName, String softwareName){
+        if (this.hardwareComponents.containsKey(hardwareName)) {
+            Hardware hardware = this.hardwareComponents.get(hardwareName);
+            hardware.removeSoftware(softwareName);
+        }
+    }
+
+    public int getHardwareComponentsSize() {
+        return this.hardwareComponents.size();
+    }
+
+    public int getSoftwareComponentCount() {
+        return this.hardwareComponents.values().stream()
+                .mapToInt(Hardware::getSoftwareCount).sum();
+    }
+
+    public int getTotalOperationalMemoryInUse(){
+        return this.getValue(Hardware::getUsedMemory);
+    }
+
+    public int getTotalCapacityTaken(){
+        return this.getValue(Hardware::getUsedCapacity);
+    }
+
+    public int getMaximumMemory() {
+        return this.getValue(ComponentType::getSomeMemory);
+    }
+
+    public int getMaximumCapacity() {
+        return this.getValue(ComponentType::getSomeCapacity);
+    }
+
+    private int getValue (ToIntFunction<Hardware> func){
+      return   this.hardwareComponents.values()
+                .stream().mapToInt(func).sum();
     }
 }
