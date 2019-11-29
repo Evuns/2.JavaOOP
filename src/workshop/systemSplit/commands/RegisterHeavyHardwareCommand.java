058c113b962f3ea50bfa754a5753feae679e9bd3
Evuns <m_ateva@abv.bg>
2019-11-29 23:56:45 +0200

    1.SystemSplit

---
+++ src/workshop/systemSplit/commands/RegisterHeavyHardwareCommand.java
@@ -0,0 +1,30 @@
+package workshop.systemSplit.commands;
+
+import workshop.systemSplit.components.Hardware;
+import workshop.systemSplit.components.HeavyHardware;
+import workshop.systemSplit.system.SystemSplit;
+
+public class RegisterHeavyHardwareCommand extends BaseCommand{
+    private SystemSplit systemSplit;
+    private String name;
+    private int capacity;
+    private int memory;
+
+    public RegisterHeavyHardwareCommand(Object... args) {
+        super(args);
+    }
+
+    @Override
+    protected void parseArgs(Object... args) {
+        this.systemSplit = (SystemSplit)args[0];
+        this.name = args[1].toString();
+        this.capacity = Integer.parseInt(args[2].toString());
+        this.memory = Integer.parseInt(args[3].toString());
+    }
+
+    @Override
+    public void execute() {
+        Hardware hardware = new HeavyHardware(this.name, this.capacity, this.memory);
+        this.systemSplit.addHardwareComponent(hardware);
+    }
+}
