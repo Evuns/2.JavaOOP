3cca324d4fff7ecef666fcb87c338ee35f00bb93
Evuns <m_ateva@abv.bg>
2019-11-30 12:29:20 +0200

    1.SystemSplit

---
+++ src/workshop/systemSplit/commands/ReleaseSoftwareComponentCommand.java
@@ -0,0 +1,27 @@
+package workshop.systemSplit.commands;
+
+import workshop.systemSplit.components.Hardware;
+import workshop.systemSplit.system.SystemSplit;
+
+public class ReleaseSoftwareComponentCommand extends BaseCommand {
+
+    private SystemSplit systemSplit;
+    private String hardwareName;
+    private String softwareName;
+
+    public ReleaseSoftwareComponentCommand(Object... args) {
+        super(args);
+    }
+
+    @Override
+    protected void parseArgs(Object... args) {
+        this.systemSplit = (SystemSplit) args[0];
+        this.hardwareName = args[1].toString();
+        this.softwareName = args[2].toString();
+    }
+
+    @Override
+    public void execute() {
+        this.systemSplit.releaseSoftware(hardwareName,softwareName);
+    }
+}
--- src/workshop/systemSplit/commands/SystemSplitCommand.java
+++ src/workshop/systemSplit/commands/SystemSplitCommand.java
@@ -2,25 +2,21 @@ package workshop.systemSplit.commands;
 
 import workshop.systemSplit.system.SystemSplit;
 
-public class ReleasedSoftwareComponentCommand extends BaseCommand {
+public class SystemSplitCommand extends BaseCommand {
 
     private SystemSplit systemSplit;
-    private String hardwareName;
-    private String softwareName;
 
-    public ReleasedSoftwareComponentCommand(Object... args) {
+    public SystemSplitCommand(Object... args) {
         super(args);
     }
 
     @Override
     protected void parseArgs(Object... args) {
         this.systemSplit = (SystemSplit) args[0];
-        this.hardwareName = args[1].toString();
-        this.softwareName = args[2].toString();
     }
 
     @Override
     public void execute() {
-
+        System.out.println(this.systemSplit);
     }
 }
